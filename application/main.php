0ccb93141d276dc24cc859489813bcdd6c6dd86f
Victoria Vivian <vickysparkes@googlemail.com>
2011-02-20 20:13:59 +0000

    Database connection test

--- application/main.php
+++ application/main.php
@@ -9,3 +9,42 @@ try {
 } catch(PDOException $e) {
     error_log('Db Connection failure: ' . $e->getMessage());
 }
+
+$stm = $db->query("
+    SELECT 
+        r.name AS recipe_name,
+        r.method,
+        r.cook_minutes,
+        r.prepare_minutes,
+        r.serves,
+        i.name AS ingredient_name,
+        ri.quantity
+    FROM  
+        `recipe` r, 
+        `ingredient` i, 
+        `recipe_ingredient` ri
+    WHERE 
+        r.`id` = 1 
+        AND r.`id` = ri.`recipe_id` 
+        AND i.`id` = ri.`ingredient_id`
+");
+
+$stm->setFetchMode(PDO::FETCH_ASSOC);
+
+$recipe = $stm->fetchAll();
+$recipeDetail = $recipe[0];
+
+echo "<h2>" . $recipeDetail['recipe_name'] . "</h2>";
+echo "<ul>"; 
+echo "<li>Serves " . $recipeDetail['serves'] . "</li>";
+echo "<li>Preparation time: " . $recipeDetail['prepare_minutes'] . " minutes</li>";
+echo "<li>Cooking time: " . $recipeDetail['cook_minutes'] . " minutes</li>";
+echo "</ul>";
+
+echo "<ul>"; 
+foreach ($recipe as $ingredient) {
+    echo "<li>" . $ingredient['quantity'] . ' ' . $ingredient['ingredient_name'] . "</li>";
+} 
+echo "</ul>";
+
+echo "<p>" . nl2br($recipeDetail['method']) . "</p>";
--- public/index.php
+++ public/index.php
@@ -1,4 +1,3 @@
 <?php
 require_once(dirname('__FILE__') . '/../application/main.php');
 
-echo "Welcome to Cupboard Cruncher";
